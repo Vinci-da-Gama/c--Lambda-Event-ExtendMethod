937397738e8e2f9d81176491975b03733c9ea141
Vinci-da-Gama <11582145@student.uts.edu.au>
2018-02-14 10:56:30 +1100

    origin

---
+++ Lambda_Event_ExtendMethod-Solu/Lambda_Event_ExtendMethod_Pj/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda_Event_ExtendMethod_Pj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
